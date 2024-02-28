4c37f13823b911c07aad264f1935dc35158a5808
Lyndon Maydwell <lyndon@sordina.net>
2024-02-28 16:46:26 +1000

    Creation of PR to Plugins Index

--- scripts/make-plugin-index-pr.sh
+++ scripts/make-plugin-index-pr.sh
@@ -2,7 +2,7 @@
 
 set -evo pipefail
 
-ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../../)"
+ROOT="$(pwd)"
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
 make_pr_to_cli_index() {
@@ -11,20 +11,21 @@ make_pr_to_cli_index() {
     REPO='github.com/hasura/cli-plugins-index.git'
     echo "sending pr to hasura/cli-plugins-index"
 
-    export DIST_PATH="${ROOT}/cli/dist"
+    export DIST_PATH="${ROOT}/dist"
 
     configure_git
     git clone https://${REPO} ~/plugins-index
    
     cd ~/plugins-index
-    git checkout -b pro-${LATEST_TAG}
-    mkdir -p ./plugins/pro/${LATEST_TAG}
-    cp ${DIST_PATH}/manifest.yaml ./plugins/pro/${LATEST_TAG}/manifest.yaml
+    git checkout -b upgrade-from-v2-${LATEST_TAG}
+    mkdir -p ./plugins/upgrade-from-v2/${LATEST_TAG}
+    cp ${DIST_PATH}/manifest.yaml ./plugins/upgrade-from-v2/${LATEST_TAG}/manifest.yaml
     
     git add .
     git commit -m "update pro manifest to ${LATEST_TAG}"
-    git push -q https://${GITHUB_TOKEN}@${REPO} pro-${LATEST_TAG}
-    hub pull-request -f -F- <<<"update pro manifest to ${LATEST_TAG}" -r ${REVIEWERS} -a ${REVIEWERS}
+    git push -q https://${GITHUB_TOKEN}@${REPO} upgrade-from-v2-${LATEST_TAG}
+    # hub pull-request -f -F- <<<"update upgrade-from-v2 manifest to ${LATEST_TAG}" -r ${REVIEWERS} -a ${REVIEWERS}
+    hub pull-request -f -F- <<<"update upgrade-from-v2 manifest to ${LATEST_TAG}"
 
     unset DIST_PATH
 }
