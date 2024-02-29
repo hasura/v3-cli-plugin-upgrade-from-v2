795e326b96cd3dfb0b70a2c1c4950ad8852cdbc2
Lyndon Maydwell <lyndon@sordina.net>
2024-02-29 18:13:58 +1000

    Testing against master branch

--- scripts/make-plugin-index-pr.sh
+++ scripts/make-plugin-index-pr.sh
@@ -3,7 +3,9 @@
 set -evo pipefail
 
 ROOT="$(pwd)"
-LATEST_TAG=$(git describe --tags --abbrev=0 || git rev-parse --abbrev-ref HEAD)
+VERSION=$(sed -n '/version/ s/.*"\(.*\)".*/\1/p' < dist/manifest.yaml)
+
+# LATEST_TAG=$(git describe --tags --abbrev=0 || git rev-parse --abbrev-ref HEAD)
 
 make_pr_to_cli_index() {
     configure_git
@@ -13,39 +15,18 @@ make_pr_to_cli_index() {
 
     export DIST_PATH="${ROOT}/dist"
 
-    # configure_ssh
     configure_git
 
     git clone ${REPO} ~/plugins-index
    
     cd ~/plugins-index
-    # TODO: Switch to main after we check that this works
-    git checkout -b upgrade-from-v2-${LATEST_TAG}
-    mkdir -p ./plugins/upgrade-from-v2/${LATEST_TAG}
-    cp ${DIST_PATH}/manifest.yaml ./plugins/upgrade-from-v2/${LATEST_TAG}/manifest.yaml
+    # git checkout -b upgrade-from-v2-${VERSION}
+    mkdir -p ./plugins/upgrade-from-v2/${VERSION}
+    cp ${DIST_PATH}/manifest.yaml ./plugins/upgrade-from-v2/${VERSION}/manifest.yaml
     
     git add .
-    git commit -m "update pro manifest to ${LATEST_TAG}"
-    git push origin upgrade-from-v2-${LATEST_TAG}
-}
-
-configure_ssh() {
-  mkdir -p ~/.ssh
-  touch ~/.ssh/config
-  echo "${INDEX_PRIVATE_KEY}" > ~/.ssh/cli-plugins-index
-  echo "${INDEX_PUBLIC_KEY}" > ~/.ssh/cli-plugins-index.pub
-  # echo "" >> ~/.ssh/cli-plugins-index
-  # echo "# github.com/hasura/cli-plugins-index.git" >> ~/.ssh/cli-plugins-index
-  chmod 600 ~/.ssh/cli-plugins-index
-  cat <<EOF > ~/.ssh/config
-  Host github.com-cli-plugins-index
-	 HostName github.com
-   User git
-	 IdentityFile ~/.ssh/cli-plugins-index
-EOF
-
-  # Check config
-  cat ~/.ssh/cli-plugins-index | sed 's/./X/g'
+    git commit -m "update pro manifest to ${VERSION}"
+    git push origin master # upgrade-from-v2-${VERSION}
 }
 
 configure_git() {
