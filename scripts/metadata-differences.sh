5fca558d338c04d902870e598da0a9fe3d99661d
Lyndon Maydwell <lyndon@sordina.net>
2024-03-11 11:27:07 +1000

    Action header types

--- scripts/metadata-differences.sh
+++ scripts/metadata-differences.sh
@@ -21,19 +21,21 @@ set +e
 set +u
 set +o pipefail
 
+echo
+echo "Polling metadata $HASURA_V2_URL/v1/metadata for changes...."
+echo
+
 while true
 do
-  echo
-  echo
-  echo
-  echo
-  echo
   jd -color "$TEMP_DIR/1.json" "$TEMP_DIR/2.json" > "$TEMP_DIR/diff1.json"
 
   if [ $? -ne 0 ]; then
     cp "$TEMP_DIR/diff1.json" "$TEMP_DIR/diff2.json"
+    echo; echo; echo; echo; echo
+    echo "------------------------------------------------------------------------"
+    echo
+    cat "$TEMP_DIR/diff2.json"
   fi
-  cat "$TEMP_DIR/diff2.json"
 
   curl -sS "$HASURA_V2_URL/v1/metadata" -d '{"type":"export_metadata","version":2,"args":{}}' -H "x-hasura-admin-secret: $HASURA_V2_ADMIN_SECRET" > "$TEMP_DIR/1.json"
   sleep 2
